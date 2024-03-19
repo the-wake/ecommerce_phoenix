7be7fd812e985d964d04383e5c28887e5767bb13
Ben Martin <bmartin2009@gmail.com>
2024-03-19 14:42:16 -0500

    A little bit of starter database setup.

--- lib/ecommerce_web/router.ex
+++ lib/ecommerce_web/router.ex
@@ -18,6 +18,7 @@ defmodule EcommerceWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    resources "/products", ProductController
   end
 
   # Other scopes may use custom stacks.
